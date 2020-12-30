91d9195a889463a65ea3304d5c34d7e37c7a5598
chris6661 <74381468+chris6661@users.noreply.github.com>
2020-12-29 22:47:33 -0600

    added questions and variables for quiz and timer

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+//questions and answers
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
+
+var score = 0;
+var questionIndex= 0;
+
+//variables for quiz
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTimer");
+var questionsDiv = document.querySelector("#questionsDiv"); 
+var wrapper = document.querySelector("#wrapper"); 
+
+//time variables 
+var secondsLeft = 76;
+var holdInterval = 0;
+var penalty = 10; 
+var ulCreate = document.createElement("ul");
